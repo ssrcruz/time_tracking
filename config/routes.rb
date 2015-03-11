57abe511ff959ec3479ea3647adf93b06f68d321
Ruben <ssrcruz@gmail.com>
2015-03-11 09:42:17 -0400

    Create project validation test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'developers#index'
   resources :projects
   resources :developers
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
+require 'minitest'
 
 class ProjectTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "name and max hours should be present" do
+    project = Project.new
+    assert_not project.save
+  end
 end
